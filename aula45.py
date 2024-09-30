b2c8263fe70f1ddd84a8c8dde96ea0a47a3edebf
Pedro Cattete <pedro.cattete@gmail.com>
2024-09-30 16:14:22 -0300

    Att das aulas 45 e 46

--- aula45.py
+++ aula45.py
@@ -1,20 +1,28 @@
 """
 Iterável -> str, range, etc (__iter__)
 Iterador -> quem sabe entregar um valor por vez
+Método -> ação dentro de um objeto
 next -> me entregue o próximo valor
 iter -> me entregue seu iterador
 """
 # for letra in texto
 texto = 'Luiz'  # iterável
 
-# iteratador = iter(texto)  # iterator
+"""
+texto = iter ('Luiz') # __iter__()
+# "__" são chamados de dunder
+"""
 
-# while True:
-#     try:
-#         letra = next(iteratador)
-#         print(letra)
-#     except StopIteration:
-#         break
+iteratador = iter(texto)  # iterator
+while True:
+     try:
+         letra = next(iteratador)
+         print(letra)
+     except StopIteration:
+         break
+
+print("")
+# While e For -> Ambos tendo a mesma função mas com tamanhos diferentes
 
 for letra in texto:
     print(letra)
